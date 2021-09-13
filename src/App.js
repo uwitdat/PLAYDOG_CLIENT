5a6f31fe7e2f113daa43635eeb2f4ce2e4918db7
Daniel Dvorkin <dvorkin212@gmail.com>
2021-09-12 21:44:52 -0400

    got the base of the landing page setup

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import {
   Route
 } from "react-router-dom";
 import HeaderBar from './components/HeaderBar/HeaderBar';
-import FooterBar from './components/FooterBar/FooterBar'
-import HomePage from './components/pages/HomePage/HomePage'
-import EventsPage from './components/pages/EventsPage/EventsPage'
-import DogsPage from './components/pages/DogsPage/DogsPage'
-import NewEventPage from './components/pages/NewEventPage/NewEventPage'
-import LoginPage from './components/pages/LoginPage/LoginPage'
+import FooterBar from './components/FooterBar/FooterBar';
+import HomePage from './components/pages/HomePage/HomePage';
+import EventsPage from './components/pages/EventsPage/EventsPage';
+import DogsPage from './components/pages/DogsPage/DogsPage';
+import NewEventPage from './components/pages/NewEventPage/NewEventPage';
+import LoginPage from './components/pages/LoginPage/LoginPage';
+import LandingPage from './components/pages/LandingPage/LandingPage';
 
 function App() {
+  let path = window.location.pathname;
+
+
   return (
     <Router>
       <div className="App">
-        <HeaderBar />
-        <Route path='/' exact component={HomePage} />
-        <Route path='/events' component={EventsPage} />
-        <Route path='/dogs' component={DogsPage} />
-        <Route path='/new-event' component={NewEventPage} />
-        <Route path='/login' component={LoginPage} />
-        <FooterBar />
+        {(!path.includes("/welcome") && (
+          <>
+            <HeaderBar />
+            <Route path='/' exact component={HomePage} />
+            <Route path='/events' component={EventsPage} />
+            <Route path='/dogs' component={DogsPage} />
+            <Route path='/new-event' component={NewEventPage} />
+            <Route path='/login' component={LoginPage} />
+            <FooterBar />
+          </>
+        ))}
+        
+        <Route path="/welcome" component={LandingPage} />
+
       </div>
     </Router>
   );
