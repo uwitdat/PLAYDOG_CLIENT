a5c03f78070dac0d3c93e2766a75c9591918139e
Ben Shekhtman <benshekhtman@hotmail.com>
2021-09-13 00:23:33 -0400

    resolve

--- src/redux-store/reducers/errorReducers.js
+++ src/redux-store/reducers/errorReducers.js
@@ -4,16 +4,12 @@ const initialState = {
 };
 
 // eslint-disable-next-line
-<<<<<<< HEAD
-export default function(state = initialState, action) {  
-=======
-export default function(state = initialState, action) {
->>>>>>> 57329875bf6b9bd8cb3585597f378bdf3c305ac6
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return state;
     case SET_ERRORS:
-      state.errors = {...state.errors, ...action.payload}
+      state.errors = { ...state.errors, ...action.payload }
       return state;
     default:
       return state;
