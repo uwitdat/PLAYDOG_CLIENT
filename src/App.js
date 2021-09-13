54431f4b4fefd415a76cf896457ab3a0f03b1e18
Daniel Dvorkin <dvorkin212@gmail.com>
2021-09-13 00:44:49 -0400

    fixed imports on app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import {
   BrowserRouter as Router,
   Route, Switch
 } from "react-router-dom";
-import HomePage from './components/pages/HomePage/HomePage';
-import EventsPage from './components/pages/EventsPage/EventsPage';
-import DogsPage from './components/pages/DogsPage/DogsPage';
-import NewEventPage from './components/pages/NewEventPage/NewEventPage';
-import LoginPage from './components/pages/LoginPage/LoginPage';
-import LandingPage from './components/pages/LandingPage/LandingPage';
+import HomePage from 'pages/HomePage/HomePage';
+import EventsPage from 'pages/EventsPage/EventsPage';
+import DogsPage from 'pages/DogsPage/DogsPage';
+import NewEventPage from 'pages/NewEventPage/NewEventPage';
+import LoginPage from 'pages/LoginPage/LoginPage';
+import LandingPage from 'pages/LandingPage/LandingPage';
 import SignIn from "components/Auth/SignIn/SignIn";
 import SignUp from "./components/Auth/SignUp/SignUp";
 import ForgotPassword from "components/Auth/ForgotPassword/ForgotPassword";
 import { SET_WINDOW_WIDTH } from "redux-store/types";
 import { connect, useDispatch } from "react-redux";
 import { useEffect } from 'react';
-import HeaderBar from 'components/HeaderBar/HeaderBar';
-import FooterBar from 'components/FooterBar/FooterBar';
 
 function App() {
   const dispatch = useDispatch()
