b47079fd3462eb7c7e469c7e095b837b5ced9bd8
Ben Shekhtman <benshekhtman@hotmail.com>
2021-09-12 23:48:31 -0400

    conflict resolve

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route
 } from "react-router-dom";
 import HeaderBar from './components/HeaderBar/HeaderBar';
+<<<<<<< HEAD
 import FooterBar from './components/FooterBar/FooterBar';
 import HomePage from './components/pages/HomePage/HomePage';
 import EventsPage from './components/pages/EventsPage/EventsPage';
@@ -15,10 +16,22 @@ import LandingPage from './components/pages/LandingPage/LandingPage';
 function App() {
   let path = window.location.pathname;
 
+=======
+import FooterBar from './components/FooterBar/FooterBar'
+import HomePage from './pages/HomePage/HomePage'
+import EventsPage from './pages/EventsPage/EventsPage'
+import DogsPage from './pages/DogsPage/DogsPage'
+import NewEventPage from './pages/NewEventPage/NewEventPage'
+import LoginPage from './pages/LoginPage/LoginPage'
+import ProfilePage from './pages/ProfilePage/ProfilePage'
+
+function App() {
+>>>>>>> 8076434 (desktop nav, mobile nav)
 
   return (
     <Router>
       <div className="App">
+<<<<<<< HEAD
         {(!path.includes("/welcome") && (
           <>
             <HeaderBar />
@@ -33,6 +46,16 @@ function App() {
 
         <Route path="/welcome" component={LandingPage} />
 
+=======
+        <HeaderBar />
+        <Route path='/' exact component={HomePage} />
+        <Route path='/events' component={EventsPage} />
+        <Route path='/dogs' component={DogsPage} />
+        <Route path='/new-event' component={NewEventPage} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/profile' component={ProfilePage} />
+        <FooterBar />
+>>>>>>> 8076434 (desktop nav, mobile nav)
       </div>
     </Router>
   );
