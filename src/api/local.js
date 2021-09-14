37c22402ebba87346a9ea144e674b4a168a14d2d
jacobbenaim <jacobjbenaim@gmail.com>
2021-09-13 22:30:17 -0400

    Added axios helpers

---
+++ src/api/local.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const csrfTokenRegex = RegExp(/(csrftoken=.*;|csrftoken=.*)/)
+const csrfToken = document.cookie?.match(csrfTokenRegex) ? document.cookie?.match(csrfTokenRegex)[0] : ''
+const fbToken = localStorage.getItem('fb-token')
+// axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+// axios.defaults.xsrfCookieName = csrfToken;
+
+let local;
+if (fbToken !== undefined) {
+
+  const headers =  {
+    "X-CSRFToken": csrfToken,
+    "Authorization": `Token ${fbToken}`
+  }
+
+  local = axios.create({
+    baseURL: "http://localhost:8000/api",
+    withCredentials: true,
+    xsrfHeaderName: 'X-CSRFToken',
+    xsrfCookieName: 'csrftoken',
+    headers
+  });
+}
+
+export default local
