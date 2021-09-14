55ec880f4d724bb8923188cfe93ba18458725ba6
jacobbenaim <jacobjbenaim@gmail.com>
2021-09-14 17:59:21 -0400

    removed test call

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,21 +1,7 @@
-import local from 'api/local'
-import React, { useEffect } from 'react'
+import React from 'react'
 import './HomePage.css'
 
 const HomePage = () => {
-
-    const getDogs = async () => {
-        try {
-            const response = await local.get('/pets')
-
-            console.log(response)
-        } catch(err) {
-            console.log(err)
-        }
-    }
-    useEffect(() => {
-        getDogs()
-    }, [])
     return (
         <div className='HomePage__container'>
             <h1>HomePage</h1>
