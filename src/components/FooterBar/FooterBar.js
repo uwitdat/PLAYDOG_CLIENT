4d0d5b5806cb99ed44ee132bb2a53a4744fbda82
Ben Shekhtman <benshekhtman@hotmail.com>
2021-09-15 16:19:40 -0400

    fixed styling, router mobile-nav, implemented custom loader

--- src/components/FooterBar/FooterBar.js
+++ src/components/FooterBar/FooterBar.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import './FooterBar.css'
 import { FaPaw, FaRegCalendarAlt } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import { AiFillHome, AiFillPlusCircle } from "react-icons/ai";
+import { SET_ERRORS } from 'redux-store/types';
+import { useDispatch } from 'react-redux';
+import { useFirebase } from "react-redux-firebase";
 
 const linkStyle = {
     marginTop: '1rem',
@@ -12,7 +15,10 @@ const linkStyle = {
 }
 
 const FooterBar = () => {
-    let pathname = window.location.pathname
+    const firebase = useFirebase();
+    const history = useHistory();
+    const dispatch = useDispatch();
+    let pathname = window.location.href
 
     const [path, setPath] = useState('/')
 
@@ -20,13 +26,32 @@ const FooterBar = () => {
         setPath(pathname)
     }, [path, pathname])
 
+    const signOut = async () => {
+        try {
+            await firebase
+                .auth()
+                .signOut()
+
+            // Sign-out successful.
+            console.log("signed out");
+            setPath('/login')
+            history.push("/sign-in");
+        } catch (error) {
+            console.log(error);
+            dispatch({
+                type: SET_ERRORS,
+                payload: error
+            })
+        }
+    };
+
     return (
         <div className='FooterBar'>
-            <Link to='/' style={linkStyle}><span onClick={() => setPath('/')} className={path === '/' ? 'FooterBar__icon mob-active' : 'FooterBar__icon'}><AiFillHome /></span></Link>
-            <Link to='/events' style={linkStyle}><span onClick={() => setPath('/events')} className={path === '/events' ? 'FooterBar__icon mob-active' : 'FooterBar__icon'}><FaRegCalendarAlt /></span></Link>
+            <Link to='/' style={linkStyle}><span onClick={() => setPath('http://localhost:3000/PLAYDOG_CLIENT#/')} className={path === 'http://localhost:3000/PLAYDOG_CLIENT#/' ? 'FooterBar__icon mob-active' : 'FooterBar__icon'}><AiFillHome /></span></Link>
+            <Link to='/events' style={linkStyle}><span onClick={() => setPath('http://localhost:3000/PLAYDOG_CLIENT#/events')} className={path === 'http://localhost:3000/PLAYDOG_CLIENT#/events' ? 'FooterBar__icon mob-active' : 'FooterBar__icon'}><FaRegCalendarAlt /></span></Link>
             <Link to='/new-event' style={linkStyle}><span className='FooterBar__icon lrg'><AiFillPlusCircle /></span></Link>
-            <Link to='/dogs' style={linkStyle}><span onClick={() => setPath('/dogs')} className={path === '/dogs' ? 'FooterBar__icon mob-active' : 'FooterBar__icon'}><FaPaw className={path === '/dogs' ? 'rotate' : null} /></span></Link>
-            <Link to='/login' style={linkStyle}><span onClick={() => setPath('/login')} className='FooterBar__icon'><FiLogOut /></span></Link>
+            <Link to='/dogs' style={linkStyle}><span onClick={() => setPath('http://localhost:3000/PLAYDOG_CLIENT#/dogs')} className={path === 'http://localhost:3000/PLAYDOG_CLIENT#/dogs' ? 'FooterBar__icon mob-active' : 'FooterBar__icon'}><FaPaw className={path === '/dogs' ? 'rotate' : null} /></span></Link>
+            <div className='FooterBar__sign-out' to='/sign-in' style={linkStyle}><span onClick={signOut} className='FooterBar__icon'><FiLogOut /></span></div>
         </div>
     )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,23 @@
 import local from 'api/local'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from 'components/Loader/Loader'
 import './HomePage.css'
 
 const HomePage = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(function () {
+            setLoading(false);
+        }, 3000);
+    }, []);
 
     const getDogs = async () => {
         try {
             const response = await local.get('/pets')
 
             console.log(response)
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
@@ -17,8 +25,10 @@ const HomePage = () => {
         getDogs()
     }, [])
     return (
+
         <div className='HomePage__container'>
-            <h1>HomePage</h1>
+            {loading ? <Loader /> : <h1>HomePage</h1>}
+
         </div>
     )
 }
