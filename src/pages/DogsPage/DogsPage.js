658a2f190336f0e6d829b19c49edbdadeb74ebc3
Ben Shekhtman <benshekhtman@hotmail.com>
2021-09-15 20:32:23 -0400

    dogs page styling base for mobile

--- src/pages/DogsPage/DogsPage.js
+++ src/pages/DogsPage/DogsPage.js
@@ -1,16 +1,18 @@
 import local from 'api/local'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import './DogsPage.css'
+import './DogsPage.scss'
 
 const DogsPage = () => {
     const [dogs, setDogs] = useState([]);
+
+    console.log('DOGS___>', dogs)
 
     const getDogs = async () => {
         try {
             const response = await local.get('/pets')
             setDogs(response.data.results);
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
@@ -18,13 +20,17 @@ const DogsPage = () => {
         getDogs()
     }, [])
     return (
-        <div className="container">
+        <div className="Dogs">
             {dogs && dogs.map((dog) => {
                 return (
-                    <>
-                        <Link to={`/dogs/${dog.id}`} key={dog.id}>{dog.name} - {dog.breed}</Link>
-                        <br/>
-                    </>
+                    <div className='Dogs__container' key={dog.id}>
+                        <h1 className='Dogs__title'>{dog.name}</h1>
+                        <p className='Dogs__breed'>{dog.breed}</p>
+                        <p className='Dogs__age'>{dog.age} Years old</p>
+                        <img style={{ height: '20rem', width: '20rem', borderRadius: '50%', objectFit: 'cover', marginTop: '1rem' }} src='https://www.adorama.com/alc/wp-content/uploads/2018/02/shutterstock_591809333.jpg' alt='dog' />
+                        <p className='Dogs__desc'>{dog.description}</p>
+                        <div className='Dogs__line'></div>
+                    </div>
                 )
             })}
         </div>
